6067fefe4093806854134b735b5e820a8062ab8d
Kelvin-orcasita <134247750+Kelvin-orcasita@users.noreply.github.com>
2023-10-10 13:26:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+import { getPokemons } from "./pokemon.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  request(0, 20);
+});
+
+let next = 20;
+
+document.getElementById("btnNext").addEventListener("click", function () {
+  let back = 0;
+  if (next > 0 && next < 1000) {
+      next += 20;
+      back = next - 19
+      request(back, next);
+  }
+});
+
+
+const request = async (back, next)=>{
+  
+
+  try{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${back}&limit=${next}`)
+    const body = await response.json();
+
+    console.log(response);
+
+    getPokemons(body);
+
+  } catch(err){
+    console.log(err);
+  }
+
+}
---
+++ pintarPokemon.js
@@ -0,0 +1,79 @@
+export const datos = (id) => {
+
+    const ul = document.querySelector("#ul-datos")
+
+    const li = document.createElement("li")
+    const img = document.createElement("img")
+    const type = document.createElement("b")
+    const h3 = document.createElement("h4")
+    const div = document.createElement("div")
+    const divfooter = document.createElement("div")
+    const a = document.createElement("a")
+    a.setAttribute("href", `${id.id}`); 
+    
+    div.className = "card bg-dark bg-opacity-25 shadow bg-body"
+    li.className = "container-fluid card-body"
+    h3.className = "card-title my-2"
+    img.className = "card-img-top border rounded-circle p-3" 
+    type.className = "card-text text-center rounded-4 p-1 px-3"
+    divfooter.className = "d-flex justify-content-center align-items-center gap-2 fw-bold"
+    a.className = "text-decoration-none text-black"
+    
+    const tipo = id.types[0].type.name;
+    let color = "000";  
+    if (tipo == "grass") {
+    color = "#78C850";
+    } else if (tipo == "fire") {
+    color = "#F08030";
+    } else if (tipo == "water") {
+    color = "#6890F0";
+    } else if (tipo == "electric") {
+    color = "#F8D030";
+    } else if (tipo == "ice") {
+    color = "#98D8D8";
+    } else if (tipo == "fighting") {
+    color = "#C03028";
+    } else if (tipo == "poison") {
+    color = "#A040A0";
+    } else if (tipo == "ground") {
+    color = "#E0C068";
+    } else if (tipo == "flying") {
+    color = "#A890F0";
+    } else if (tipo == "psychic") {
+    color = "#F85888";
+    } else if (tipo == "bug") {
+    color = "#A8B820";
+    } else if (tipo == "rock") {
+    color = "#B8A038";
+    } else if (tipo == "ghost") {
+    color = "#705898";
+    } else if (tipo == "dragon") {
+    color = "#7038F8";
+    } else if (tipo == "dark") {
+    color = "#705848";
+    } else if (tipo == "steel") {
+    color = "#B8B8D0";
+    } else if (tipo == "fairy") {
+    color = "#EE99AC";
+    } else if (tipo == "normal") {
+    color = "#A8A878";
+    }
+
+    
+    img.src = id.sprites.other.home.front_default
+    h3.textContent = `${id.id} - ${id.name}`.toUpperCase();
+    type.textContent = `${id.types[0].type.name}`.toUpperCase();
+    divfooter.textContent = `Type:`
+
+    img.style.backgroundColor = color + "93";
+    type.style.backgroundColor = color;
+
+    li.appendChild(a)
+    ul.appendChild(div)
+    div.appendChild(li)
+    a.appendChild(img)
+    a.appendChild(h3)
+    divfooter.appendChild(type)
+    a.appendChild(divfooter)
+}
+  
